1e14133b7c6a69fce18bc709987c2d4d8d5ae76b
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-10-17 13:40:08 +0200

    bruk text istedet for varchar

--- src/main/resources/db/migration/V1.0.9__microfrontends.sql
+++ src/main/resources/db/migration/V1.0.9__microfrontends.sql
@@ -1,10 +1,10 @@
 create table if not exists microfrontends(
     id serial primary key,
     initiated_time timestamp not null,
-    ident varchar(11) not null,
-    action varchar(15) not null,
-    microfrontend_id varchar(50) not null,
-    initiated_by varchar(50)
+    ident text not null,
+    action text not null,
+    microfrontend_id text not null,
+    initiated_by text
 );
 
 
